f752fcbd70eb789b3ecc79a6b7382e3e41a8f2aa
smeth194243 <smor7432@740s-pl-114.local>
2016-09-14 13:32:49 -0600

    Just experimenting in Xcode.

---
+++ Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var thisIsANumber = 32423
+print(thisIsANumber)
+
+var num1 = 1
+var num2 = 2
+print(num1+num2)
+
+//FIRST(This is a comment)
+/*This is also a comment,
+but it is written over multiple
+lines. */
+
+
+let cat = "🐱";
+//Prints "🐱"
+
+print(cat);
+
+
+var meaningOfLife = 42
+print(meaningOfLife)
+
+var num3 = 1+2+3+4+5+6+7+8+9
+print(num3=num2+num1);
+
+
+
+
+
+
+
+
+
+
